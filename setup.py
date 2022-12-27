e18909ff5416897337522000d9d4ed979369bd8f
ritesh <riteshkvt.jsr@gmail.com>
2022-12-27 15:05:40 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import find_packages, setup
 
-def get_requirements():
-    pass
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+def get_requirements() ->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        #This will read the text file line by line and return list 
+        requirement_list = requirement_file.readlines() 
+        #In the returned list we will  have \n values which have been used in text file to display
+        #packages in different lines, we will replace them
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
 
 setup(
     name = "sensor",
